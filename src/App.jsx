f85852826275f8fe3cb791023613b9f81b59bde5
MicheleSaraceni <michele.saraceni23@email.com>
2025-04-01 17:47:24 +0200

    fixed output

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,14 @@ export default function App() {
 
 
   getDashboardData("london")
-    .then((data) => console.log(data))
+    .then((data) => {
+      console.log('Dasboard data:', data);
+      console.log(
+        `${data.city} is in ${data.country}.\n` +
+        `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n` +
+        `The main airport is ${data.airport}.\n`
+      );
+    })
     .catch((error) => console.log("Error fetching data:", error))
 
 }
